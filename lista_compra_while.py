237fd8d3c1ec90a6c70ecc51c786d0165f28e45e
JGM <jngutierrezmorales@gmail.com>
2018-01-27 12:17:33 +0100

    Primera lista creada utilizando for in

---
+++ lista_compra_while.py
@@ -0,0 +1,15 @@
+
+mi_lista = []
+input_usuario = input("¿Que necesitas comprar? (escribe FIN para salir): ")
+
+while input_usuario != "FIN":
+    mi_lista.append(input_usuario)
+    input_usuario = input("¿Que necesitas comprar? (escribe FIN para salir): ")
+
+largo_lista = len(mi_lista)
+indice_actual = 0
+
+for item in mi_lista:
+    print("Tengo que comprar {}".format(item))
+
+print("Esta es la lista de la compra")
