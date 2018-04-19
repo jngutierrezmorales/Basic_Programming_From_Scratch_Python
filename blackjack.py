2232a8eef7ce9fb218b41fe0a5b4b8a47a57a31a
JGM <jngutierrezmorales@gmail.com>
2018-04-19 12:59:12 +0200

    Juego Blackjack

--- blackjack.py
+++ blackjack.py
@@ -5,6 +5,9 @@ class Card:
         self.number = number
         self.suit = suit
 
+    def __str__(self):
+        return "{} of {}".format(self.number, self.suit)
+
 
 class Deck:
     suits = ["diamonds", "heart", "spades", "clubs"]
@@ -20,3 +23,27 @@ class Deck:
     def give_random_card(self):
         pos = 0
         return self.cards.pop(pos)
+
+    def __str__(self):
+        str_cards = [str(card) for card in self.cards]
+        return "Deck with {} cards:\n{}".format(len(self.cards), ",\n".join(str_cards))
+
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.score = 0
+
+
+class Game:
+    def __init__(self):
+        deck = Deck()
+        player = Player()
+
+    def run(self):
+        pass
+
+
+if __name__ == "__main__":
+    deck = Deck()
+    print(deck)
